f0019fc2721ccfc571b849a2b7c4575c423c5853
Puneet-chauhan01 <pschauhan6149@gmail.com>
2023-08-20 21:28:16 +0530

    completed task 1 and task 2

--- new.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import numpy as np
-
-img=cv2.imread('1.png')
-lower_grn= np.array([35,50,50])
-up_grn=np.array([85,255,255])
-lower_brwn= np.array([35,50,50])
-up_grn=np.array([85,255,255])
